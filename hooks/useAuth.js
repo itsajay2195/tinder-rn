8cf7adcd7393d4c1a3992b1ebbe705d7e6432d25
itsajay2195 <ajaykumar.95@yahoo.com>
2022-04-02 10:44:57 +0530

    initializing the context api

--- hooks/useAuth.js
+++ hooks/useAuth.js
@@ -1,11 +1,16 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-export const AuthProvider = () => {
+const AuthContext =  React.createContext({
+    //This is the initial state
+
+})  /*This is the thing data layer */
+
+export const AuthProvider = ({children}) => {
   return (
-    <View>
-      <Text>useAuth</Text>
-    </View>
+    <AuthContext.Provider value = {null}>
+      {children}
+    </AuthContext.Provider>
   )
 }
 
